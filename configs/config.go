77400f7b2f2077de4fc73ae3c328b6bf5beb93c6
Andre Flor <andreflor96@hotmail.com>
2024-10-24 22:21:19 -0300

    Add .gitignore, configs/config.go, and go.mod files

---
+++ configs/config.go
@@ -0,0 +1,40 @@
+package configs
+
+import (
+	"github.com/go-chi/jwtauth"
+	"github.com/spf13/viper"
+)
+
+var cfg *conf
+
+type conf struct {
+	DBDriver string
+	DBHost string
+	DBPort string
+	DBUser string
+	DBPassword string
+	DBName string
+	WebServerPort string
+	JwtSecret string
+	JwtExpiresIn int
+	TokenAuth *jwtauth.JWTAuth
+}
+
+func LoadConfig(path string) (*conf, error) {
+	viper.SetConfigName("app_config")
+	viper.SetConfigType("env")
+	viper.AddConfigPath(path)
+	viper.SetConfigFile(".env")
+	viper.AutomaticEnv()
+	err := viper.ReadInConfig()
+	if err != nil {
+		panic(err)
+	}
+	err = viper.Unmarshal(&cfg)
+	if err != nil {
+		panic(err)
+	}
+	cfg.TokenAuth = jwtauth.New("HS256", []byte(cfg.JwtSecret), nil)
+	
+	return cfg, nil
+}
