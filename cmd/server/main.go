9ac9a4c2ec1fd2f676a854b9f65aea0fb33c53c6
Andre Flor <andreflor96@hotmail.com>
2024-10-26 13:46:30 -0300

    Add main.go, config.go, user.go, user_test.go, and id.go files

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,5 +1,30 @@
 package main
 
+import (
+	"net/http"
+
+	"github.com/andreflor21/go-api/configs"
+	"github.com/andreflor21/go-api/internal/entity"
+	"github.com/andreflor21/go-api/internal/infra/database"
+	"github.com/andreflor21/go-api/internal/infra/webserver/handlers"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
 func main(){
-	
+	_, err := configs.LoadConfig(".")
+	if err != nil {
+		panic(err)
+	}
+
+	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+	db.AutoMigrate(&entity.User{}, &entity.Product{})
+	productDB := database.NewProduct(db)
+	productHandler := handlers.NewProductHandler(productDB)
+	http.HandleFunc("/products", productHandler.CreateProduct)
+	http.ListenAndServe(":8000", nil)
 }
+
